5a67032c97cdbca11a86a16cbf4cc658cc066705
naman2001jain <62708894+naman2001jain@users.noreply.github.com>
2020-10-04 09:26:36 +0530

    Create printing_tokens.c
    printing tokens hacker rank question on c.

---
+++ printing_tokens.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    int i=0;
+    while(i<strlen(s)){
+        if(s[i]!=' '){
+            printf("%c",s[i]);
+        }
+        if(s[i]==' '){
+            printf("\n");
+        }
+        i++;
+    }
+    return 0;
+}
